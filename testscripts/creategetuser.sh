b2757a4d11c5930039099a0832a3c1afa839fe42
blakehulett7 <blake.hulett7@gmail.com>
2024-09-10 17:08:18 -0500

    renamed repo

--- testscripts/creategetuser.sh
+++
@@ -1,7 +0,0 @@
-echo &&
-    echo "Creating user blakehulett..." &&
-    echo &&
-    curl \
-        --header "Content-Type: application/json" \
-        --data '{"username":"blakehulett"}' \
-        http://localhost:8080/v1/users
